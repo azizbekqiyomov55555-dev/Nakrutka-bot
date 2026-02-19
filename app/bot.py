d1d4769de4016c41e88cca7c0a8dc367c964f6be
azizbekqiyomov55555-dev <azizbekqiyomov55555@gmail.com>
2026-02-19 12:05:16 +0500

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -1,23 +1,8 @@
 import asyncio
-import os
-from aiohttp import web
-
-async def handle(request):
-    return web.Response(text="Bot ishlayapti")
 
 async def main():
     print("Bot ishga tushdi")
 
-    app = web.Application()
-    app.router.add_get("/", handle)
-
-    port = int(os.environ.get("PORT", 8000))
-
-    runner = web.AppRunner(app)
-    await runner.setup()
-
-    site = web.TCPSite(runner, "0.0.0.0", port)
-    await site.start()
-
     while True:
-        await asyncio.sleep(3600)
+        print("Ishlayapti...")
+        await asyncio.sleep(30)
