982a9f65f4c9038b98f33e1601c49a43a7711042
azizbekqiyomov55555-dev <azizbekqiyomov55555@gmail.com>
2026-02-19 12:08:18 +0500

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -1,7 +1,21 @@
 import asyncio
+from aiohttp import web
+
+async def handle(request):
+    return web.Response(text="Bot ishlayapti")
+
+async def start_web_server():
+    app = web.Application()
+    app.router.add_get("/", handle)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", 8000)
+    await site.start()
 
 async def main():
     print("Bot ishga tushdi")
+
+    await start_web_server()
 
     while True:
         print("Ishlayapti...")
