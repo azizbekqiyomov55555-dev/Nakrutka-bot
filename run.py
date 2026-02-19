4862c200eb151f586028df25e11d78f583f0e8de
azizbekqiyomov55555-dev <azizbekqiyomov55555@gmail.com>
2026-02-19 12:25:23 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 import asyncio
-from app.bot import main
+from app.bot import dp, bot
+from app.db import init_db
+
+async def main():
+    await init_db()
+    await dp.start_polling(bot, skip_updates=True)
 
 if __name__ == "__main__":
     asyncio.run(main())
